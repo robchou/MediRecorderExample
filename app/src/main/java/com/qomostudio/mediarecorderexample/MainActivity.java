ff80a965d4c27c8f9afd238298f92c846c9d1d6a
Robin Chou <robin.m.chou@gmail.com>
2016-05-25 09:15:52 +0800

    Video Recorder

--- app/src/main/java/com/qomostudio/mediarecorderexample/MainActivity.java
+++ app/src/main/java/com/qomostudio/mediarecorderexample/MainActivity.java
@@ -2,23 +2,34 @@ package com.qomostudio.mediarecorderexample;
 
 import android.app.Activity;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.ImageButton;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String outputFilePath = Environment.getExternalStorageDirectory().
+            getAbsolutePath() + "/output.mp4";
+
 
     Camera mCamera;
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
     ImageButton captureButton;
+    MediaRecorder mMediaRecorder;
+
+    volatile boolean isRecordingStared = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +53,23 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     private void initViews() {
         captureButton = (ImageButton)findViewById(R.id.btn_capture);
+        captureButton.setOnClickListener(new CaptureListener());
         surfaceView = (SurfaceView)findViewById(R.id.sv_preview);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    class CaptureListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (isRecordingStared) {
+                releaseMediaRecorder();
+            } else {
+                prepareVideoRecorder();
+                mMediaRecorder.start();
+            }
+        }
     }
 
     public static Camera getCameraInstance(int cameraId) {
@@ -58,6 +82,56 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         }
 
         return c;
+    }
+
+    private boolean prepareVideoRecorder() {
+        if (mCamera == null) {
+            mCamera = getCameraInstance(Camera.CameraInfo.CAMERA_FACING_BACK);
+            setCameraDisplayOrientation(this, Camera.CameraInfo.CAMERA_FACING_BACK, mCamera);
+        }
+        mMediaRecorder = new MediaRecorder();
+
+        mCamera.unlock();
+        mMediaRecorder.setCamera(mCamera);
+
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+//        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+//        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+//        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+
+        mMediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+        File outputFile = new File(outputFilePath);
+        if (outputFile.exists()) {
+            outputFile.delete();
+        }
+
+        mMediaRecorder.setOutputFile(outputFilePath);
+
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            Log.e(TAG, "IlleagalStateException prepare MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "IOException preparing MeidaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void releaseMediaRecorder() {
+        if (mMediaRecorder != null) {
+            mMediaRecorder.reset();
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+            mCamera.lock();
+        }
     }
 
     public static void setCameraDisplayOrientation(Activity activity,
